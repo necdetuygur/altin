d5f27a723f59fa3cb1e4f1c95e33d8bf887a6e0b
necdetuygur <necdet.uygur@gmail.com>
2020-12-31 12:22:15 +0300

    Create redirect-https.js

---
+++ redirect-https.js
@@ -0,0 +1,1 @@
+(function(){ if (location.protocol !== 'https:') { var httpsTestUrl = window.location.href.replace("http:", "https:") + "json"; var xhr = new XMLHttpRequest(); xhr.onreadystatechange = function() { if(this.status === 200){ location.replace(`https:${location.href.substring(location.protocol.length)}`); } }; xhr.open("GET", httpsTestUrl, true); xhr.send(); } })(); 
