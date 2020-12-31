86af871e64d07e9b7c89d481dc207c480328200d
necdetuygur <necdet.uygur@gmail.com>
2020-12-31 12:26:48 +0300

    Update redirect-https.js

--- redirect-https.js
+++ redirect-https.js
@@ -1,1 +1,3 @@
-(function(){ if (location.protocol !== 'https:') { var httpsTestUrl = window.location.href.replace("http:", "https:") + "json"; var xhr = new XMLHttpRequest(); xhr.onreadystatechange = function() { if(this.status === 200){ location.replace(`https:${location.href.substring(location.protocol.length)}`); } }; xhr.open("GET", httpsTestUrl, true); xhr.send(); } })(); 
+while (location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
